a44635e30f19b27517412c2cfe8a372c3be86afb
JessicaJuliet <jessica.akomfrah@gmail.com>
2022-11-12 14:15:13 +0000

    Link JS & add nav item underline

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+function activeNavItem() {
+    var navItem = document.querySelectorAll(".nav-item");
+    for (let i = 0; i < navItem.length; i++) {
+        navItem[i].addEventListener("click", function() {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+        });
+    }
+}
+activeNavItem();
