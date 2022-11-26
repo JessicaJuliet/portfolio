ee788a333d934ff5c54eea1344129a7223634513
JessicaJuliet <jessica.akomfrah@gmail.com>
2022-11-26 17:43:06 +0000

    Separate overlay div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 // Index js
 
 const hamburgerIcon = document.getElementById("hamburger-icon");
-var navMenu = document.getElementById("nav");
+var navMenu = document.querySelector("#nav div.overlay");
+var navItems = document.querySelector("nav .content");
 var navItem = document.querySelectorAll(".nav-item");
 
 
 // Toggle nav menu
 hamburgerIcon.addEventListener("click", function() {
   navMenu.classList.toggle("active");
+  navItems.classList.toggle("active");
 })
 
 // Close nav menu on link click
